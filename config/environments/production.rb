3975d58e5c074b29dc946dd0c706d2867416b713
David Wen <dstwen@gmail.com>
2014-10-17 12:08:01 -0700

    devise settings for dev and prod environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise Settings
+  config.action_mailer.default_url_options = { :host => 'http://six-degrees-app.herokuapp.com' }
+
+  # Gmail Settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'six-degrees-app.herokuapp.com',
+    user_name:            'dstwen@gmail.com',
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
