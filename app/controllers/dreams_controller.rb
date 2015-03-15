e31b80b4743a2a4fe48cffe024444c1fd7bc1dfa
David Wen <dstwen@gmail.com>
2015-03-14 20:52:00 -0700

    API for dreams#show #create #update #destroy

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -5,4 +5,56 @@ class DreamsController < ApplicationController
       format.json { render :json => dreams }
     end
   end
+
+  def show
+    respond_to do |format|
+      dream = Dream.find(params[:id])
+      format.json { render :json => dream }
+    end
+  end
+
+  def create
+    respond_to do |format|
+      dream = Dream.new(dream_params)
+      if dream.save
+        format.json { render :json => { :status => 200, :message => "dream created successfully", :dream => dream } }
+      else
+        format.json { render :json => { :status => 500, :message => dream.errors.full_messages.join(". ") + "." } }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      dream = Dream.find(params[:id])
+      dream.assign_attributes(dream_params)
+      if dream.save
+        format.json { render :json => { :status => 200, :message => "dream updated successfully", :dream => dream } }
+      else
+        format.json { render :json => { :status => 500, :message => dream.errors.full_messages.join(". ") + "." } }
+      end
+    end
+  end
+
+  def destroy
+    respond_to do |format|
+      dream = Dream.find(params[:id])
+      dream.destroy
+      format.json { render :json => { :status => 200, :message => "dream destroyed successfully", :dream => dream } }
+    end
+  end
+
+  private 
+
+  def dream_params
+    params.require(:dream).permit(
+      :description,
+      :dream_type,
+      :created_at,
+      :updated_at,
+      :user_id,
+      :_destroy
+    )
+  end
+
 end
