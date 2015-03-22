e7102b0dcb79327c741a88d557ef23404b78f96c
David Wen <dstwen@gmail.com>
2015-03-21 18:05:54 -0700

    add facebook capture first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
       user.image = auth.info.image # assuming the user model has an image
     end
   end
