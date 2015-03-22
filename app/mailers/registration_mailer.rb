03917c4613e00ed0d21c0725da7b329c8e36c6cd
David Wen <dstwen@gmail.com>
2015-03-22 14:36:30 -0700

    change text for default from email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -2,7 +2,7 @@ class RegistrationMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 
-  default from: '"Raise Change Team" <sixdegress@gmail.com>'
+  default from: '"Six Degrees Team" <sixdegress@gmail.com>'
 
   def confirmation_instructions(resource, token, opts={})
     opts[:subject] = "Confirm your Six Degrees account"
