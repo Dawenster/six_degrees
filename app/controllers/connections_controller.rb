dbd6c27ee29f43fdb631224fee0b9cdc807bff69
David Wen <dstwen@gmail.com>
2015-03-22 13:50:22 -0700

    emails sent when connection offered and connection made

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,21 +1,27 @@
 class ConnectionsController < ApplicationController
   def create
     respond_to do |format|
-      @connection = Connection.new(connection_params)
-      @connection.user_id = current_user.id
-      if @connection.save
-        format.json { render :json => { :status => 200, :message => "connection created successfully", :connection => @connection } }
-        format.html do
-          flash[:notice] = "Help successfully offered!"
-          redirect_to dreams_path
+      begin
+        @connection = Connection.new(connection_params)
+        @connection.user_id = current_user.id
+        if @connection.save
+          ConnectionMailer.connection_offered(@connection.id).deliver
+          format.json { render :json => { :status => 200, :message => "connection created successfully", :connection => @connection } }
+          format.html do
+            flash[:notice] = "Help successfully offered!"
+            redirect_to dreams_path
+          end
+        else
+          connection_errors = @connection.errors.full_messages.join(". ") + "."
+          format.json { render :json => { :status => 500, :message => connection_errors } }
+          format.html do
+            flash[:alert] = connection_errors
+            redirect_to dreams_path
+          end
         end
-      else
-        connection_errors = @connection.errors.full_messages.join(". ") + "."
-        format.json { render :json => { :status => 500, :message => connection_errors } }
-        format.html do
-          flash[:alert] = connection_errors
-          redirect_to dreams_path
-        end
+      rescue => error
+        format.json { render :json => { :status => 500, :message => error } }
+        format.html { render :json => { :status => 500, :message => error } }
       end
     end
   end
@@ -25,6 +31,7 @@ class ConnectionsController < ApplicationController
       begin
         connection = Connection.find(params[:id])
         connection.update_attributes(:accepted => true)
+        ConnectionMailer.connection_made(connection.id).deliver
         format.json { render :json => { :status => 200, :message => "connection accepted successfully", :connection => connection } }
       rescue => error
         format.json { render :json => { :status => 500, :message => error } }
---
+++ app/mailers/connection_mailer.rb
@@ -0,0 +1,25 @@
+class ConnectionMailer < ActionMailer::Base
+  helper :application # gives access to all helpers defined within `application_helper`.
+
+  default from: '"Six Degrees Team" <sixdegrees@gmail.com>'
+
+  def connection_offered(connection_id)
+    @connection = Connection.find(connection_id)
+    @dream = @connection.dream
+    @helper = @connection.user
+    @receiver = @dream.user
+
+    @url = activities_url(@receiver)
+    mail(from: @helper.email, to: @receiver.email, subject: "#{@helper.first_name} wants to help with your dream")
+  end
+
+  def connection_made(connection_id)
+    @connection = Connection.find(connection_id)
+    @dream = @connection.dream
+    @helper = @connection.user
+    @receiver = @dream.user
+
+    @url = activities_url(@helper)
+    mail(from: @receiver.email, to: @helper.email, subject: "#{@receiver.first_name} accepted your offer to help")
+  end
+end
