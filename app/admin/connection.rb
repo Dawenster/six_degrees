2a92a1582a47f3bcbb6b69da01ab7d66784590e0
David Wen <dstwen@gmail.com>
2015-04-09 19:57:01 -0700

    add columns to admin connections

--- app/admin/connection.rb
+++ app/admin/connection.rb
@@ -13,6 +13,30 @@ ActiveAdmin.register Connection do
   #   permitted << :other if resource.something?
   #   permitted
   # end
-
-
+  index :as => ActiveAdmin::Views::IndexAsTable do
+    column "ID" do |connection|
+      link_to connection.id, admin_connection_path(connection)
+    end
+    column :initial_message
+    column "Dream" do |connection|
+      link_to connection.dream.description, admin_dream_path(connection.dream)
+    end
+    column "Helper" do |connection|
+      if connection.user.nil?
+        "N/A"
+      else
+        link_to connection.user.full_name, admin_user_path(connection.user)
+      end
+    end
+    column "Receiver" do |connection|
+      if connection.dream.user.nil?
+        "N/A"
+      else
+        link_to connection.dream.user.full_name, admin_user_path(connection.dream.user)
+      end
+    end
+    column :accepted
+    column :created_at
+    actions
+  end
 end
