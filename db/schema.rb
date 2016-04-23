21d0c91655dc55f3127754e29fb359455a2ee4de
David Wen <dstwen@gmail.com>
2016-04-23 16:42:58 -0700

    Create tags table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606205820) do
+ActiveRecord::Schema.define(version: 20160423234022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,12 @@ ActiveRecord::Schema.define(version: 20150606205820) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "recipient_id"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
