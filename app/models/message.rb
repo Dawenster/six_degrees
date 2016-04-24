a075652954eb21448a2ed912be264ae284edc9e4
David Wen <dstwen@gmail.com>
2016-04-24 08:43:44 -0700

    Add message and tag db validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
   belongs_to :recipient, :class_name => 'User', :foreign_key => 'recipient_id'
 
   belongs_to :dream
+
+  validates :content, presence: true, allow_blank: false
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@ class Tag < ActiveRecord::Base
   has_and_belongs_to_many :dreams, uniq: true
 
   scope :alphabetical, -> { order(:name) }
+
+  validates :name, presence: true, allow_blank: false
 end
