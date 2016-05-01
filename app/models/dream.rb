fcc68fd8564c429203bdd1f0e92a3d448a2f78fa
David Wen <dstwen@gmail.com>
2016-05-01 08:44:34 -0700

    Remove uniq true between dreams and tags

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -3,7 +3,7 @@ class Dream < ActiveRecord::Base
   has_many :connections
   has_many :messages, :dependent => :destroy
   has_many :hearts, :dependent => :destroy
-  has_and_belongs_to_many :tags, uniq: true
+  has_and_belongs_to_many :tags
 
   validates :description, :user_id, :presence => true, allow_blank: false
   validate :has_at_least_one_tag
