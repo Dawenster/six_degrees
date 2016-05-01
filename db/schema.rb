b974ba3539cb8047f8843c886a30cc422f5acb59
David Wen <dstwen@gmail.com>
2016-05-01 15:11:24 -0700

    Add referred by user id to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160501220510) do
+ActiveRecord::Schema.define(version: 20160501221056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 20160501220510) do
     t.datetime "avatar_updated_at"
     t.boolean  "admin",                  default: false
     t.boolean  "message_notification",   default: true
+    t.integer  "referred_by_user_id"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
