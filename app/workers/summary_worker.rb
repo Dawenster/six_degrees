c33177216b1257768a40d0c93251991602300b0d
David Wen <dstwen@gmail.com>
2016-05-08 12:00:38 -0700

    Only send auto summary emails if env variable says so

--- app/workers/summary_worker.rb
+++ app/workers/summary_worker.rb
@@ -12,10 +12,12 @@ class SummaryWorker
 
   def perform
     existing_summary_helper_ids = Summary.all.map(&:helper_id)
-    Connection.accepted.where.not(user_id: existing_summary_helper_ids).each do |connection|
-      next unless connection.dream
-      if connection.updated_at.to_date + 1.week == Time.current.to_date || connection.updated_at.to_date + 2.weeks == Time.current.to_date || connection.updated_at.to_date + 3.week == Time.current.to_date
-        SummaryMailer.write_summary(connection.dream.user.id, connection.dream.id, connection.user.id).deliver
+    if ENV["AUTO_SUMMARY_EMAILS"] == "true"
+      Connection.accepted.where.not(user_id: existing_summary_helper_ids).each do |connection|
+        next unless connection.dream
+        if connection.updated_at.to_date + 1.week == Time.current.to_date || connection.updated_at.to_date + 2.weeks == Time.current.to_date || connection.updated_at.to_date + 3.week == Time.current.to_date
+          SummaryMailer.write_summary(connection.dream.user.id, connection.dream.id, connection.user.id).deliver
+        end
       end
     end
   end
