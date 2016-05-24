e0bf9cb540a067547e639022dc5b75833ae929ea
David Wen <dstwen@gmail.com>
2016-05-24 00:41:33 -0700

    Modify production.rb sendgrid settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -106,7 +106,8 @@ Rails.application.configure do
     domain = "gosixdegrees.com"
   end
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
     :domain => domain,
